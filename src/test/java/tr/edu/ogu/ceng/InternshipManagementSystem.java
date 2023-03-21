5f230de1c17b40d30376b590738247893df73faf
Yusuf Kartal <yusuf.kartal@avkaryazilim.com.tr>
2023-03-21 14:47:15 +0300

    Initial commit

---
+++ src/test/java/tr/edu/ogu/ceng/InternshipManagementSystem.java
@@ -0,0 +1,13 @@
+package tr.edu.ogu.ceng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipManagementSystem {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipManagementSystem.class, args);
+	}
+	
+}
