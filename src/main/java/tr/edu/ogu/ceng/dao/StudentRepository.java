46f4b795a95f4dd1ad5032ff4a2681814a7dfbc1
Yusuf Kartal <yusuf.kartal@avkaryazilim.com.tr>
2023-03-21 18:03:03 +0300

    Project folder tree created

---
+++ src/main/java/tr/edu/ogu/ceng/dao/StudentRepository.java
@@ -0,0 +1,5 @@
+package tr.edu.ogu.ceng.dao;
+
+public class StudentRepository {
+
+}
---
+++ src/main/java/tr/edu/ogu/ceng/model/Student.java
@@ -0,0 +1,24 @@
+package tr.edu.ogu.ceng.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "ims_student")
+@Data
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(length = 20, name = "student_no", nullable = false)
+	private String studentNo;
+	
+}
