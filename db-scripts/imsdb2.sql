6dfa00163c4cbf5589ce35bf0da5540bbfe55aa7
ezgi <ezgiozdikyar@gmail.com>
2023-04-01 17:08:26 +0300

    Setting tablosu için db scripti güncellendi.

--- db-scripts/imsdb2.sql
+++ db-scripts/imsdb2.sql
@@ -6,7 +6,8 @@ ALTER TABLE ims_faculty_supervisors DROP COLUMN email;
 ALTER TABLE ims_users ADD COLUMN email varchar NOT NULL;
 ALTER TABLE ims_users ADD CONSTRAINT ims_users_email_un UNIQUE (email);
 
-ALTER TABLE ims_settings ADD COLUMN mail_host varchar NOT NULL;
-ALTER TABLE ims_settings ADD COLUMN mail_port integer NOT NULL;
-ALTER TABLE ims_settings ADD COLUMN mail_username varchar NOT NULL;
-ALTER TABLE ims_settings ADD COLUMN mail_password varchar NOT NULL;
+INSERT INTO ims_settings (key)
+VALUES ('mail_host'),
+       ('mail_port'),
+       ('mail_username'),
+       ('mail_password');
