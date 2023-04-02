356de465d05cb10bc38928b3695d868040a6e2e6
Ergün Altındağ <127850980+ergunaltindag@users.noreply.github.com>
2023-04-02 15:47:13 +0300

    Update StudentService.java
    StudentService fixed

--- src/main/java/tr/edu/ogu/ceng/service/StudentService.java
+++ src/main/java/tr/edu/ogu/ceng/service/StudentService.java
@@ -2,18 +2,15 @@ package tr.edu.ogu.ceng.service;
 
 import org.springframework.stereotype.Service;
 
+import lombok.AllArgsConstructor;
 import tr.edu.ogu.ceng.dao.StudentRepository;
 import tr.edu.ogu.ceng.model.Student;
 
 @Service
+@AllArgsConstructor
 public class StudentService {
 
 	private final StudentRepository studentRepository;
-
-	public StudentService(StudentRepository studentRepository) {
-		super();
-		this.studentRepository = studentRepository;
-	}
 
 	public Student getStudent(int id) {
 		return studentRepository.findById(id).orElse(null);
