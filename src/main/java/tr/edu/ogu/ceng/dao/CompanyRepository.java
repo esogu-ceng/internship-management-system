9697e28bfa1bb9144bc4163af937a7f36bbc8afe
Yağız Harman <yagizharman4biz@gmail.com>
2023-04-04 00:48:24 +0300

    Bosluk duzeltildi

--- src/main/java/tr/edu/ogu/ceng/dao/CompanyRepository.java
+++ src/main/java/tr/edu/ogu/ceng/dao/CompanyRepository.java
@@ -7,6 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import  tr.edu.ogu.ceng.model.Company;
 
 public interface CompanyRepository extends JpaRepository<Company, Long> {
-
 	public List<Company> findByNameContainingIgnoreCase(String searchString);
 }
