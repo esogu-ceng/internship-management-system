31e5b3b4e665582d5e97a2b3a8493301c96db5c4
Yusuf Kartal <yusuf.kartal@avkaryazilim.com.tr>
2023-04-03 17:16:01 +0300

    Compile errors fixed

--- src/main/java/tr/edu/ogu/ceng/service/Exception/EntityNotFoundException.java
+++ src/main/java/tr/edu/ogu/ceng/service/Exception/EntityNotFoundException.java
@@ -1,7 +1,7 @@
 package tr.edu.ogu.ceng.service.Exception;
 
 @SuppressWarnings("serial")
-public class EntityNotFoundException extends Exception{
+public class EntityNotFoundException extends RuntimeException{
 	
 	public EntityNotFoundException(String message){
 		super(message);
