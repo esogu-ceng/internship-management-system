f793e24c5639bddb9fc57fafa53ad1876e6c67a2
Mükrime Sağıroğlu <102221568+mukrime-s@users.noreply.github.com>
2023-04-10 22:10:13 +0300

    updated settings dao

--- src/main/java/tr/edu/ogu/ceng/dao/SettingsRepository.java
+++ src/main/java/tr/edu/ogu/ceng/dao/SettingsRepository.java
@@ -3,5 +3,6 @@ package tr.edu.ogu.ceng.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tr.edu.ogu.ceng.model.Settings;
 
-public interface SettingsRepository extends JpaRepository<Settings, Long> {
+public interface SettingsRepository extends JpaRepository<Settings, String> {
+	Settings findByKey(String key);
 }
