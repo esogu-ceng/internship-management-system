d5a76412c9cd5911b9abb41d8c5a93a2e95d10cb
Mükrime Sağıroğlu <102221568+mukrime-s@users.noreply.github.com>
2023-04-13 23:52:59 +0300

    unnecessary constructor deleted

--- src/main/java/tr/edu/ogu/ceng/service/Exception/AccessDeniedException.java
+++ src/main/java/tr/edu/ogu/ceng/service/Exception/AccessDeniedException.java
@@ -3,12 +3,7 @@ package tr.edu.ogu.ceng.service.Exception;
 @SuppressWarnings("serial")
 public class AccessDeniedException extends ServiceException {
 
-    public AccessDeniedException(String message) {
-        super(message);
-    }
-
     public AccessDeniedException() {
         super("Access to the requested resource is denied.");
     }
-
 }
