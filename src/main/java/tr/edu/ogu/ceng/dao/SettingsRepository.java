953cb3536777608c3684528fe137ceb4b02a3bdb
Özge <152120191071@ogrenci.ogu.edu.tr>
2023-04-14 11:08:01 +0300

    StudentService'e log mesajları eklendi.

--- src/main/java/tr/edu/ogu/ceng/dao/SettingsRepository.java
+++ src/main/java/tr/edu/ogu/ceng/dao/SettingsRepository.java
@@ -1,8 +1,9 @@
 package tr.edu.ogu.ceng.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import tr.edu.ogu.ceng.model.Settings;
 
-public interface SettingsRepository extends JpaRepository<Settings, String> {
+public interface SettingsRepository extends JpaRepository<Settings, Long> {
 	Settings findByKey(String key);
 }
