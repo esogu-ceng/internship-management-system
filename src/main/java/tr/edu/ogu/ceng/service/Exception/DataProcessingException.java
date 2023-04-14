8176feeaa4b926ecbf8789d7937f9cec77023a30
mukrime-s <mkrimesagrglu@gmail.com>
2023-04-14 21:58:35 +0300

    unnecessary constructors removed

--- src/main/java/tr/edu/ogu/ceng/service/Exception/DataProcessingException.java
+++ src/main/java/tr/edu/ogu/ceng/service/Exception/DataProcessingException.java
@@ -3,12 +3,7 @@ package tr.edu.ogu.ceng.service.Exception;
 @SuppressWarnings("serial")
 public class DataProcessingException extends ServiceException {
 
-	public DataProcessingException(String message) {
-		super(message);
-	}
-
 	public DataProcessingException() {
 		super("An error occurred while processing the data.");
 	}
-
 }
--- src/main/java/tr/edu/ogu/ceng/service/Exception/OperationNotAllowedException.java
+++ src/main/java/tr/edu/ogu/ceng/service/Exception/OperationNotAllowedException.java
@@ -2,13 +2,8 @@ package tr.edu.ogu.ceng.service.Exception;
 
 @SuppressWarnings("serial")
 public class OperationNotAllowedException extends ServiceException {
-
-	public OperationNotAllowedException(String message) {
-		super(message);
-	}
-
+	
 	public OperationNotAllowedException() {
 		super("Error!!The requested operation is not allowed.");
 	}
-
 }
