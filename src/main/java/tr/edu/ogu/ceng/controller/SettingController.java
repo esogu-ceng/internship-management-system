1675e9a47a54ef3d2f74f80da6346e1af9174062
Mükrime Sağıroğlu <102221568+mukrime-s@users.noreply.github.com>
2023-04-14 15:43:40 +0300

    try catch removed

--- src/main/java/tr/edu/ogu/ceng/controller/SettingController.java
+++ src/main/java/tr/edu/ogu/ceng/controller/SettingController.java
@@ -1,9 +1,6 @@
 package tr.edu.ogu.ceng.controller;
 
-import javax.persistence.EntityNotFoundException;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tr.edu.ogu.ceng.model.Setting;
@@ -17,21 +14,13 @@ public class SettingController {
 
 	@PutMapping
 	public ResponseEntity<Setting> updateSetting(@RequestBody Setting setting) {
-	    try {
-	        Setting updatedSetting = settingService.updateSetting(setting);
-	        return ResponseEntity.ok(updatedSetting);
-	    } 
-	    catch (EntityNotFoundException ex) {
-	        return ResponseEntity.notFound().build();
-	    } 
-	    catch (Exception ex) {
-	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-	    }
+	    Setting updatedSetting = settingService.updateSetting(setting);
+	    return ResponseEntity.ok(updatedSetting);
 	}
 
 	@GetMapping("/{key}")
-	public ResponseEntity<Setting> getSetting(@PathVariable(name = "key") String key) throws EntityNotFoundException {
-	    Setting setting = settingService.getSetting(key);
-	    return ResponseEntity.ok(setting);
+	public ResponseEntity<Setting> getSetting(@PathVariable(name = "key") String key) {
+		Setting setting = settingService.getSetting(key);
+		return ResponseEntity.ok(setting);
 	}
 }
