dc6bcf22bd0cd6d578bc2fc68ac4689c378d7461
Soner Özaşık <sonerozasik@gmail.com>
2023-04-15 20:24:45 +0300

    InternshipDto used in Internship Update
    Internship Update controller gets and returns InternshipDto.
    Mapper used in InternshipService

--- src/main/java/tr/edu/ogu/ceng/service/InternshipService.java
+++ src/main/java/tr/edu/ogu/ceng/service/InternshipService.java
@@ -2,41 +2,52 @@ package tr.edu.ogu.ceng.service;
 
 import java.sql.Timestamp;
 import java.util.Optional;
+
 import javax.persistence.EntityNotFoundException;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import tr.edu.ogu.ceng.dao.InternshipRepository;
+import tr.edu.ogu.ceng.dto.InternshipDto;
 import tr.edu.ogu.ceng.model.Internship;
 
 @Service
 public class InternshipService {
 	@Autowired
 	private InternshipRepository internshipRepository;
+
 	public Internship addInternship(Internship internship) {
 		return internshipRepository.save(internship);
 	}
-	public Internship updateInternship(Internship internship) {
-		if (!internshipRepository.existsById(internship.getId())) throw new EntityNotFoundException("Internship not found!");
-		
+
+	public InternshipDto updateInternship(InternshipDto internshipDto) {
+		if (!internshipRepository.existsById(internshipDto.getId()))
+			throw new EntityNotFoundException("Internship not found!");
+
+		ModelMapper modelMapper = new ModelMapper();
+		Internship internship = modelMapper.map(internshipDto, Internship.class);
+
 		Timestamp localDateTime = new Timestamp(System.currentTimeMillis());
 		internship.setUpdateDate(localDateTime);
-		
-		return internshipRepository.save(internship);
+
+		Internship updatedInternship = internshipRepository.save(internship);
+		return modelMapper.map(updatedInternship, InternshipDto.class);
 	}
-	
+
 	public Optional<Internship> getInternship(Long id) {
-		if (!internshipRepository.existsById(id)) throw new EntityNotFoundException("Internship not found!");
-		
+		if (!internshipRepository.existsById(id))
+			throw new EntityNotFoundException("Internship not found!");
+
 		return internshipRepository.findById(id);
 	}
-	
+
 	public boolean deleteInternship(Long id) {
-		if (!internshipRepository.existsById(id)) return false;
+		if (!internshipRepository.existsById(id))
+			return false;
 		internshipRepository.deleteById(id);
 		return true;
 	}
 
-	
 }
