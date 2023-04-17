f359cf73084dd62dc4e6e73e2807803681277c85
zeeynep <zeynepgurluzer@gmail.com>
2023-04-18 01:21:33 +0300

    InternshipRegisteryService logs added.

--- src/main/java/tr/edu/ogu/ceng/service/InternshipRegistryService.java
+++ src/main/java/tr/edu/ogu/ceng/service/InternshipRegistryService.java
@@ -10,11 +10,11 @@ import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-
+import lombok.extern.slf4j.Slf4j;
 import tr.edu.ogu.ceng.dao.InternshipRegistryRepository;
 import tr.edu.ogu.ceng.dto.InternshipRegistryDto;
 import tr.edu.ogu.ceng.model.InternshipRegistry;
-
+@Slf4j
 @Service
 @AllArgsConstructor
 @NoArgsConstructor
@@ -26,9 +26,11 @@ public class InternshipRegistryService {
 
 	public boolean deleteInternshipRegistry(long id) {
 		if (!internshipRegistryRepository.existsById(id)) {
+			log.warn("Internship registry with ID {} not found", id);
 			return false;
 		}
 		internshipRegistryRepository.deleteById(id);
+		log.info("Internship registry deleted with id: {}", id);
 		return true;
 	}
 
@@ -36,11 +38,18 @@ public class InternshipRegistryService {
 		InternshipRegistry internshipRegistry = modelMapper.map(internshipRegistryDto, InternshipRegistry.class);
 		if (!internshipRegistryRepository.existsById(internshipRegistry.getId()))
 			throw new EntityNotFoundException("Internship Registry not found!");
-
+			log.warn("Internship registry with ID {} not found", internshipRegistry.getId());
+			
 		internshipRegistry.setCreateDate(internshipRegistryRepository.getById(internshipRegistry.getId()).getCreateDate());
 		internshipRegistry.setUpdateDate(new Timestamp(System.currentTimeMillis()));
-
-		internshipRegistry = internshipRegistryRepository.save(internshipRegistry);
+		
+		try {
+			internshipRegistry = internshipRegistryRepository.save(internshipRegistry);
+            log.info("Internship registry updated: {}", internshipRegistry);
+        } catch (Exception e) {
+            log.error("Error occurred while updating internship registry: {}", e.getMessage());
+            throw e;
+        }
 		return modelMapper.map(internshipRegistry, InternshipRegistryDto.class);
 	}
 
@@ -50,8 +59,13 @@ public class InternshipRegistryService {
 		internshipRegistry.setCreateDate(localDateTime);
 		internshipRegistry.setUpdateDate(localDateTime);
 		
-
-		internshipRegistry = internshipRegistryRepository.save(internshipRegistry);
+		try {
+			internshipRegistry = internshipRegistryRepository.save(internshipRegistry);
+            log.info("Internship registry saved: {}", internshipRegistry);
+        } catch (Exception e) {
+            log.error("Error occurred while saving internship registry: {}", e.getMessage());
+            throw e;
+        }
 
 		return modelMapper.map(internshipRegistry, InternshipRegistryDto.class);
 	}
