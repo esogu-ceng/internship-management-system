c88238a8fee03aefd663867c8fd60f903bf7bc6d
Serdar Demirtaş <serdardemirtas25@gmail.com>
2023-04-17 18:46:08 +0300

    FacultySupervisorService adding test has been added

--- src/main/java/tr/edu/ogu/ceng/model/FacultySupervisor.java
+++ src/main/java/tr/edu/ogu/ceng/model/FacultySupervisor.java
@@ -6,11 +6,17 @@ import javax.persistence.*;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "ims_faculty_supervisors")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class FacultySupervisor {
 
 	@Id
