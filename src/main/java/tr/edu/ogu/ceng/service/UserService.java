4e2a9b2ba85060ca606729dcc833770225f680d4
Melisa Özel <56073734+Melisaozzel@users.noreply.github.com>
2023-04-17 18:45:32 +0300

    UserDto ve UserTypeDto eklendi
    UserDto ve UserTypeDto eklendi,
    Controllerlarında dönüş tipleri Dto olacak şekilde güncellendi.

--- src/main/java/tr/edu/ogu/ceng/service/UserService.java
+++ src/main/java/tr/edu/ogu/ceng/service/UserService.java
@@ -4,48 +4,56 @@ import java.sql.Timestamp;
 
 import javax.persistence.EntityNotFoundException;
 
-import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import lombok.extern.slf4j.Slf4j;
 import tr.edu.ogu.ceng.dao.UserRepository;
+import tr.edu.ogu.ceng.dto.UserDto;
 import tr.edu.ogu.ceng.model.User;
 
 @Slf4j
 @Service
 public class UserService {
-    @Autowired
-    private UserRepository userRepository;
+	@Autowired
+	private UserRepository userRepository;
 
-    public Page<User> getAllUsers(Pageable pageable) {
-        try {
-            log.info("Getting all users with pageable: {}", pageable);
-            Page<User> users = userRepository.findAll(pageable);
-            if (users.isEmpty()) {
-                log.warn("No users found.");
-            }
-            return users;
-        } catch (Exception e) {
-            log.error("An error occurred while getting users: {}", e.getMessage());
-            throw e;
-        }
-    }
+	public Page<UserDto> getAllUsers(Pageable pageable) {
+		try {
+			ModelMapper modelMapper = new ModelMapper();
+			log.info("Getting all users with pageable: {}", pageable);
+			Page<User> users = userRepository.findAll(pageable);
 
-    public User saveUser(User user) {
-        try {
-            Timestamp localDateTime = new Timestamp(System.currentTimeMillis());
-            user.setCreateDate(localDateTime);
-            user.setUpdateDate(localDateTime);
-            User savedUser = userRepository.save(user);
-            log.info("User saved successfully with id: {}", savedUser.getId());
-            return savedUser;
-        } catch (Exception e) {
-            log.error("An error occurred while saving user: {}", e.getMessage());
-            throw e;
-        }
-    }
+			if (users.isEmpty()) {
+				log.warn("No users found.");
+			}
+			Page<UserDto> userDtos = users.map(user -> modelMapper.map(user, UserDto.class));
+			return userDtos;
+		} catch (Exception e) {
+			log.error("An error occurred while getting users: {}", e.getMessage());
+			throw e;
+		}
+	}
+
+	public UserDto saveUser(UserDto userDto) {
+		try {
+
+			ModelMapper modelMapper = new ModelMapper();
+			User user = modelMapper.map(userDto, User.class);
+			user.setCreateDate(new Timestamp(System.currentTimeMillis()));
+			user.setUpdateDate(new Timestamp(System.currentTimeMillis()));
+			user.setId((long) 0);
+			User savedUser = userRepository.save(user);
+			log.info("User saved successfully with id: {}", savedUser.getId());
+			return modelMapper.map(savedUser, UserDto.class);
+		} catch (Exception e) {
+			log.error("An error occurred while saving user: {}", e.getMessage());
+			throw e;
+		}
+	}
 
     public User findByEmail(String email) {
         try {
