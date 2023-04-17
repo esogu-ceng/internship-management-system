492bf577b6c2c59d5391b1566f574517be292286
Yunus Akyol <yunusakyol5@gmail.com>
2023-04-17 21:34:00 +0300

    Testing Add Function for Internship Registry Service
    Internship Registry ekleme fonksiyonu için test işlemi gerçekleştirildi. Testin koşulabilmesi için gerekli lombok eklemeleri yapıldı.

--- src/main/java/tr/edu/ogu/ceng/model/Internship.java
+++ src/main/java/tr/edu/ogu/ceng/model/Internship.java
@@ -8,7 +8,8 @@ import lombok.*;
 @Entity
 @Table(name = "ims_internships")
 @Data
-
+@AllArgsConstructor
+@NoArgsConstructor
 public class Internship {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ src/test/java/tr/edu/ogu/ceng/service/InternshipRegistryTest.java
@@ -0,0 +1,53 @@
+package tr.edu.ogu.ceng.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.sql.Timestamp;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import tr.edu.ogu.ceng.dao.InternshipRegistryRepository;
+import tr.edu.ogu.ceng.model.Internship;
+import tr.edu.ogu.ceng.model.InternshipRegistry;
+
+public class InternshipRegistryTest {
+
+	@Mock
+	InternshipRegistryRepository internshipRegistryRepository;
+	InternshipRegistryService internshipRegistryService;
+
+	@BeforeEach
+	public void init() {
+		MockitoAnnotations.initMocks(this);
+		internshipRegistryService = new InternshipRegistryService(internshipRegistryRepository);
+	}
+
+	@Test
+	public void should_save_oneValid_internshipRegistry() {
+		var internshipRegistryToSave = InternshipRegistry.builder().id(1L).filePath("C:/Users/root/test")
+				.name("internshipRegistry1").type("pdf").date(new Timestamp(2023, 04, 12, 0, 0, 0, 0))
+				.createDate(new Timestamp(2023, 04, 17, 0, 0, 0, 0)).internship(new Internship()).build();
+
+		when(internshipRegistryRepository.save(any(InternshipRegistry.class))).thenReturn(internshipRegistryToSave);
+
+		var actualIR = internshipRegistryService.addInternshipRegistry(internshipRegistryToSave);
+
+		assertNotNull(actualIR);
+		assertEquals(internshipRegistryToSave.getId(), actualIR.getId());
+		assertEquals(internshipRegistryToSave.getFilePath(), actualIR.getFilePath());
+		assertEquals(internshipRegistryToSave.getName(), actualIR.getName());
+		assertEquals(internshipRegistryToSave.getType(), actualIR.getType());
+		assertEquals(internshipRegistryToSave.getDate(), actualIR.getDate());
+		assertEquals(internshipRegistryToSave.getCreateDate(), actualIR.getCreateDate());
+		assertEquals(internshipRegistryToSave.getInternship(), actualIR.getInternship());
+
+		verify(internshipRegistryRepository).save(internshipRegistryToSave);
+	}
+}
