51d84da23b5f95d4f5c57e7522663ba288ecfdfb
Ergün Altındağ <127850980+ergunaltindag@users.noreply.github.com>
2023-04-17 23:49:23 +0300

    setting and FacultySupervisior dto added
    setting and FacultySupervisior dto added

---
+++ src/main/java/tr/edu/ogu/ceng/dto/FacultySupervisorDto.java
@@ -0,0 +1,11 @@
+package tr.edu.ogu.ceng.dto;
+
+public class FacultySupervisorDto {
+
+	private Long id;
+	private String name;
+	private String surname;
+	private String phoneNumber;
+	private String supervisorNo;
+
+}
