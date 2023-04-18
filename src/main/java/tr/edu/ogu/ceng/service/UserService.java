3305d5e66691cfa6b10ee9a9a9fded9a889f4bb3
Özge <152120191071@ogrenci.ogu.edu.tr>
2023-04-18 22:29:34 +0300

    Tüm servislerdeki silme işleminde silinecek öge bulunmadığında false dönecek şekilde ortaklaştı.

--- src/main/java/tr/edu/ogu/ceng/service/UserService.java
+++ src/main/java/tr/edu/ogu/ceng/service/UserService.java
@@ -5,13 +5,12 @@ import java.sql.Timestamp;
 import javax.persistence.EntityNotFoundException;
 
 import org.modelmapper.ModelMapper;
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import tr.edu.ogu.ceng.dao.UserRepository;
 import tr.edu.ogu.ceng.dto.UserDto;
@@ -58,32 +57,32 @@ public class UserService {
 		}
 	}
 
-    public User findByEmail(String email) {
-        try {
-            log.info("Getting user by email: {}", email);
-            User user = userRepository.findByEmail(email);
-            if (user == null) {
-                log.warn("No user found with email: {}", email);
-            }
-            return user;
-        } catch (Exception e) {
-            log.error("An error occurred while getting user by email: {}", e.getMessage());
-            throw e;
-        }
-    }
+	public User findByEmail(String email) {
+		try {
+			log.info("Getting user by email: {}", email);
+			User user = userRepository.findByEmail(email);
+			if (user == null) {
+				log.warn("No user found with email: {}", email);
+			}
+			return user;
+		} catch (Exception e) {
+			log.error("An error occurred while getting user by email: {}", e.getMessage());
+			throw e;
+		}
+	}
 
-    public boolean deleteUser(long id) {
-        try {
-            if (!userRepository.existsById(id)) {
-                log.warn("User not found with id: {}", id);
-                throw new EntityNotFoundException("User Not Found!");
-            }
-            userRepository.deleteById(id);
-            log.info("User deleted successfully with id: {}", id);
-            return true;
-        } catch (Exception e) {
-            log.error("An error occurred while deleting user with id: {}: {}", id, e.getMessage());
-            throw e;
-        }
-    }
+	public boolean deleteUser(long id) {
+		try {
+			if (!userRepository.existsById(id)) {
+				log.warn("User not found with id: {}", id);
+				throw new EntityNotFoundException("User Not Found!");
+			}
+			userRepository.deleteById(id);
+			log.info("User deleted successfully with id: {}", id);
+			return true;
+		} catch (Exception e) {
+			log.error("An error occurred while deleting user with id: {}: {}", id, e.getMessage());
+			return false;
+		}
+	}
 }
