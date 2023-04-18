e1ed142f4f80b7ab8daba94ad5d386e39fbd829b
ezgi <ezgiozdikyar@gmail.com>
2023-04-18 23:42:28 +0300

    Compiler bug fix

--- src/main/java/tr/edu/ogu/ceng/service/SettingService.java
+++ src/main/java/tr/edu/ogu/ceng/service/SettingService.java
@@ -4,6 +4,7 @@ import java.sql.Timestamp;
 
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
