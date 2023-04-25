96afe3832ccc69d63fdf77f30c364b27e46adab0
Eşref Burak Kaya <ebkaya.0637@gmail.com>
2023-04-25 21:59:56 +0300

    conflict resolved in FacultySupervisorDto.java

--- src/main/java/tr/edu/ogu/ceng/dto/FacultySupervisorDto.java
+++ src/main/java/tr/edu/ogu/ceng/dto/FacultySupervisorDto.java
@@ -1,15 +1,16 @@
 package tr.edu.ogu.ceng.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Builder;
 
 @Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class FacultySupervisorDto {
+
 	private Long id;
 	private String name;
 	private String surname;
@@ -17,4 +18,5 @@ public class FacultySupervisorDto {
 	private String supervisorNo;
 	private UserDto user;
 	private FacultyDto faculty;
+
 }
