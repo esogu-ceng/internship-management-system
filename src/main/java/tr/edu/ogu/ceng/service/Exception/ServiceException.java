98aebb40302ea670b51c8605bdff20339d1b6c28
Yusuf Kartal <yusuf.kartal@avkaryazilim.com.tr>
2023-04-26 13:55:44 +0300

    AOP örnek eklendi, Advice örnek eklendi. İstinsa için ServiceException code eklendi. Controller Validation örnek eklendi.

--- src/main/java/tr/edu/ogu/ceng/service/Exception/ServiceException.java
+++ src/main/java/tr/edu/ogu/ceng/service/Exception/ServiceException.java
@@ -1,8 +1,15 @@
 package tr.edu.ogu.ceng.service.Exception;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @SuppressWarnings("serial")
+@Getter
+@Setter
 public class ServiceException extends RuntimeException {
-	
+
+	private String code;
+
 	public ServiceException(String message) {
 		super(message);
 	}
