7c6c135a502e8fe756d3971386c679df977c992a
Efekan Sargın <sarginefekan@gmail.com>
2023-05-02 00:28:49 +0300

    StudentRepository existsBy fonksiyonları eklendi

--- src/main/java/tr/edu/ogu/ceng/dao/StudentRepository.java
+++ src/main/java/tr/edu/ogu/ceng/dao/StudentRepository.java
@@ -3,9 +3,14 @@ package tr.edu.ogu.ceng.dao;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import tr.edu.ogu.ceng.model.Student;
 
 public interface StudentRepository extends JpaRepository<Student, Long>{
     Page<Student> findByNameOrSurnameOrStudentNo(String keyword1, String keyword2, String keyword3, Pageable pageable);
+	
+	boolean existsByTckn(String tcno);
+	
+	boolean existsByStudentNo(String studentno);
 }
