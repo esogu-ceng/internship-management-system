bf52c552aa4c685f985b6901bb3e27a274df81f9
mukrime-s <mkrimesagrglu@gmail.com>
2023-05-20 21:25:49 +0300

    added footer and header for student file

--- clients/student/src/index.tsx
+++ clients/student/src/index.tsx
@@ -1,13 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
-const root = ReactDOM.createRoot(
-	document.getElementById("root") as HTMLElement
-);
-root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
