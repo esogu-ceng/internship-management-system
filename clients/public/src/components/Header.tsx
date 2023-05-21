4f2a555d613a1f99c64c251b6d229fbf97ef03b8
mukrime-s <mkrimesagrglu@gmail.com>
2023-05-21 18:17:03 +0300

    updated public folder

--- clients/public/src/components/Header.tsx
+++ clients/public/src/components/Header.tsx
@@ -5,10 +5,6 @@ function Header() {
     <header>
       <div className="header-content">
         <h1 className="header-title">Staj Yönetim Sistemi</h1>
-        <div className="header-buttons">
-          <button className="header-button">Profil</button>
-          <button className="header-button">Çıkış</button>
-        </div>
       </div>
     </header>
   );
