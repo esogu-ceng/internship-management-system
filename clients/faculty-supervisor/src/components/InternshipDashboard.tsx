745eac44da1c9c04d2172e94a35a6aa138f689e2
mukrime-s <mkrimesagrglu@gmail.com>
2023-05-22 13:08:10 +0300

    added catds for faculty supervisor

---
+++ clients/faculty-supervisor/src/components/InternshipDashboard.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function InternshipDashboard() {
+  return (
+    <div className="dashboard-container">
+      <div className="dashboard-card">
+        <h2>Staj Yapan Öğrenciler</h2>{/* TODO */}
+      </div>
+      <div className="dashboard-card">{/* TODO */}
+        <h2>Geçmiş Stajlar</h2>
+      </div>
+      <div className="dashboard-card">{/* TODO */}
+        <h2>Onay Bekleyen Staj Günlükleri</h2>
+      </div>
+    </div>
+  );
+}
+
+export default InternshipDashboard;
