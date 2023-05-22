dbbedc64ab53ed8229342d867cc99eec1c830110
melihbucak <bckmelih@gmail.com>
2023-05-22 13:51:09 +0300

    Sifrenizi mi unuttunuz eklendi

--- clients/public/src/App.tsx
+++ clients/public/src/App.tsx
@@ -1,65 +1,23 @@
 import React, {useState} from 'react';
 import {toast, ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import {BrowserRouter, Route, Routes,} from 'react-router-dom';
+import Login from "./components/Login";
 
 function App() {
 
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const handleSubmit = (event: { preventDefault: () => void; }) => {
-        event.preventDefault();
-        var formData = new FormData();
-        formData.append("username", username);
-        formData.append("password", password);
-
-        fetch('/login', {
-            method: 'POST',
-            body: formData
-        })
-            .then((response) => {
-                    console.log(response)
-                    if (response.redirected === true) {
-                        window.location.href = response.url
-                    } else
-                        toast.error('Kullanıcı adı ya da parola yanlış!', {autoClose: 2000});
-                }
-            ).catch((error) => {
-
-            toast.error("Bir hata oluştu!", {autoClose: 2000})
-            console.log(error);
-        });
-    };
-
     return (
         <div className="container">
+            <Header />
             <ToastContainer/>
-            <form onSubmit={handleSubmit}>
-                <div className="form-group required">
-                    <label htmlFor="exampleInputEmail1">Kullanıcı adı</label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        id="exampleInputEmail1"
-                        aria-describedby="emailHelp"
-                        value={username}
-                        onChange={(event) => setUsername(event.target.value)}
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="exampleInputPassword1">Şifre</label>
-                    <input
-                        type="password"
-                        className="form-control"
-                        id="exampleInputPassword1"
-                        value={password}
-                        onChange={(event) => setPassword(event.target.value)}
-                    />
-                </div>
-
-                <button type="submit" className="btn btn-primary">
-                    Giriş
-                </button>
-            </form>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Login/>}/>
+                </Routes>
+            </BrowserRouter>
+            <Footer />
         </div>
     );
 }
