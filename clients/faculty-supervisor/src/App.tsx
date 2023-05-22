479676984186df9508659a48fc29d5f7aa151d2b
Mükrime Sağıroğlu <102221568+mukrime-s@users.noreply.github.com>
2023-05-22 16:16:35 +0300

    Added first screen for students and faculty-supervisor with css (#225)
    * Added first screen for students with css
    
    * color change has been made
    
    * added catds for faculty supervisor

--- clients/faculty-supervisor/src/App.tsx
+++ clients/faculty-supervisor/src/App.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import InternshipDashboard from "./components/InternshipDashboard";
+
 import "./index.css";
 
 function App() {
 	return (
 	  <React.StrictMode>
 		<Header />
-		<div>
-		</div>
+		<div className="app-container">
+        	<InternshipDashboard />
+      	</div>
 		<Footer />
 	  </React.StrictMode>
 	);
