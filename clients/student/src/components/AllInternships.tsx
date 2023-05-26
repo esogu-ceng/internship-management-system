074fc190ec1d9ff8fd49badd87f9197036ff2f7f
ykenanaksan <ykenanaksan@gmail.com>
2023-05-26 20:03:01 +0300

    Fix Internship Document

--- clients/student/src/components/AllInternships.tsx
+++ clients/student/src/components/AllInternships.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Internship } from '../types/InternshipType';
-import InternshipDocument from './InternshipDocuments'; //TODO Internship Documents
 import Modal from '../components/CompanyInfo';
 
 
