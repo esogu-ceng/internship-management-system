86670ead039a44ba5bd2bca3e07c68eda0ef4afb
Yunus Akyol <yunusakyol5@gmail.com>
2023-05-27 02:16:09 +0300

    AdminDashboard v0.1 Added
    AdminDashboard v0.1 added for developments on other admin screens.

---
+++ clients/admin/src/components/AdminDashboard.tsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+
+function AdminDashboard() {
+  return (
+    <div className="dashboard-container">
+      <div className="dashboard-card">
+        <h2>Öğrenciler</h2>
+        {/* TODO */}
+      </div>
+      <div className="dashboard-card">
+        <h2>Şirket Sorumluları</h2>
+        {/* TODO */}
+      </div>
+      <div className="dashboard-card">
+        <h2>Fakülte Sorumluları</h2>
+        {/* TODO */}
+      </div>
+      <div className="dashboard-card">
+        <h2>Ayarlar</h2>
+        {/* TODO */}
+      </div>
+    </div>
+  );
+}
+
+export default AdminDashboard;
