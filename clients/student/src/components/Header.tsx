c00768b25d1e767b0c196e5c14c69079ef9834f3
OmerKurtuldu <85666770+OmerKurtuldu@users.noreply.github.com>
2023-05-28 16:47:19 +0300

    Login cors hatası yusuf hocayla çözüldü

--- clients/student/src/components/Header.tsx
+++ clients/student/src/components/Header.tsx
@@ -1,13 +1,26 @@
 import React from "react";
+import {toast, ToastContainer} from "react-toastify";
 
 function Header() {
+
+  const handleLogout = () => {
+      window.location.href = process.env.REACT_APP_API_BASE_URI + "logout";
+  };
+
+  /*const handleLogout = () => {
+    window.location.href = process.env.PUBLIC_URL + "/logout";
+  };*/
+  
+  
   return (
     <header>
       <div className="header-content">
         <h1 className="header-title">Staj Yönetim Sistemi</h1>
         <div className="header-buttons">
           <button className="header-button">Profil</button>
-          <button className="header-button">Çıkış</button>
+          <button className="header-button" onClick={handleLogout}>
+            Çıkış
+          </button>
         </div>
       </div>
     </header>
