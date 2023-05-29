45df7732e4aa23cd77e3045d850d4e35765cde71
Ergün Altındağ <127850980+ergunaltindag@users.noreply.github.com>
2023-05-29 21:42:47 +0300

    Update InternshipDashboard.tsx

--- clients/student/src/components/InternshipDashboard.tsx
+++ clients/student/src/components/InternshipDashboard.tsx
@@ -2,25 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function InternshipDashboard() {
-  const root_path : string | undefined = process.env.PUBLIC_URL
   return (
     <div className="dashboard-container">
       <div className="dashboard-card">
-        <Link to={`${root_path}/AllInternships`}>
+        <Link to="/student/AllInternships">
           <h2>Tüm Stajlarım</h2>
         </Link>
       </div>
       <div className="dashboard-card">
-<<<<<<< Updated upstream
-        <Link to="/student/StudentInfo">
-          <h2>Öğrenci Bilgilerim</h2>
-        </Link>
-=======
          <Link to="/StudentInfo">
           <h2>Öğrenci Bilgilerim</h2>
         </Link>
-        {/* TODO */}
->>>>>>> Stashed changes
       </div>
       <div className="dashboard-card">
         {/* TODO */}
