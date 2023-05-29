03732d44bbd593b0c2b882507ef54ce7096dbd42
huseyingulcuu <huseyingulcuu@gmail.com>
2023-05-29 18:18:48 +0300

    feat: Create a custom hook to manage the supervisor.

---
+++ clients/admin/src/hooks/useSupervisorManagement.ts
@@ -0,0 +1,181 @@
+/** @format */
+
+import { useEffect, useState } from "react";
+
+import {
+  CompanySuperviserUpdate,
+  CompanySupervisor,
+  CompanySupervisorCreate,
+  CompanySupervisorPagedResponse,
+  Pageable,
+} from "../types/CompanySupervisors";
+
+const useSupervisorManagement = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const [isAddModalOpen, setIsAddModalOpen] = useState<boolean>(false);
+  const [isUpdateModalOpen, setIsUpdateModalOpen] = useState<boolean>(false);
+  const [selectedCompanySupervisor, setSelectedCompanySupervisor] =
+    useState<CompanySupervisor>();
+
+  const [companySupervisors, setCompanySupervisors] = useState<
+    CompanySupervisor[]
+  >([]);
+  const [pagination, setPagination] = useState<Pageable>();
+
+  const getAllCompanySupervisors = async (
+    pageNo: number = 0,
+    limit: number = 10,
+    sortBy: string = "name"
+  ) => {
+    setLoading(true);
+
+    const errorMessage = "Error retrieving company supervisors.";
+
+    try {
+      const url = `http://localhost:8080/api/company-supervisor?pageNo=${pageNo}&limit=${limit}&sortBy=${sortBy}`;
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(errorMessage);
+      }
+
+      const data: CompanySupervisorPagedResponse = await response.json();
+
+      setCompanySupervisors(data.content);
+      setPagination({
+        totalElements: data.totalElements,
+        totalPages: data.totalPages,
+        number: data.number,
+        size: data.size,
+      });
+    } catch (error) {
+      setError(errorMessage);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const addCompanySupervisor = async (request: CompanySupervisorCreate) => {
+    setLoading(true);
+    setError(null);
+    const errorMessage = "Error adding company supervisor.";
+    try {
+      const url = "http://localhost:8080/api/company-supervisor";
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(request),
+      });
+
+      if (!response.ok) {
+        throw new Error(errorMessage);
+      }
+
+      getAllCompanySupervisors(pagination?.number || 0);
+    } catch (error) {
+      setError(errorMessage);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const updateCompanySupervisor = async (request: CompanySuperviserUpdate) => {
+    setLoading(true);
+    setError(null);
+    const errorMessage = "Error updating company supervisor.";
+    try {
+      const url = "http://localhost:8080/api/company-supervisor";
+      const response = await fetch(url, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(request),
+      });
+
+      if (!response.ok) {
+        throw new Error(errorMessage);
+      }
+
+      getAllCompanySupervisors(pagination?.number || 0);
+    } catch (error) {
+      setError(errorMessage);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const deleteCompanySupervisor = async (id: number) => {
+    setLoading(true);
+    setError(null);
+    const errorMessage = "Error deleting company supervisor.";
+    try {
+      const url = `http://localhost:8080/api/company-supervisor/${id}`;
+      const response = await fetch(url, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(errorMessage);
+      }
+
+      if (!pagination) return;
+      const updatedTotalItems = pagination.totalElements - 1;
+      const updatedTotalPages = Math.ceil(updatedTotalItems / pagination.size);
+
+      if (companySupervisors.length === 1 && pagination.number > 0) {
+        getAllCompanySupervisors(pagination.number - 1);
+      } else if (pagination.number >= updatedTotalPages) {
+        getAllCompanySupervisors(updatedTotalPages - 1);
+      } else {
+        getAllCompanySupervisors(pagination.number);
+      }
+    } catch (error) {
+      setError(errorMessage);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const pageChangeHandler = (pageNumber: number) => {
+    if (!pagination) return;
+
+    setPagination({ ...pagination, number: pageNumber });
+    getAllCompanySupervisors(pageNumber);
+  };
+
+  useEffect(() => {
+    getAllCompanySupervisors();
+  }, []);
+
+  return {
+    loading,
+    error,
+    isAddModalOpen,
+    isUpdateModalOpen,
+    selectedCompanySupervisor,
+    companySupervisors,
+    pagination,
+    setIsAddModalOpen,
+    getAllCompanySupervisors,
+    addCompanySupervisor,
+    updateCompanySupervisor,
+    deleteCompanySupervisor,
+    pageChangeHandler,
+    setIsUpdateModalOpen,
+    setSelectedCompanySupervisor,
+  };
+};
+
+export default useSupervisorManagement;
