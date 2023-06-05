4865f35beaaf4b1f69be2576efef61917363352a
Bora Soylu <mail@borasoylu.com>
2023-06-05 23:28:47 +0300

    company supervisor header css fixed

--- clients/company-supervisor/src/components/Footer.tsx
+++ clients/company-supervisor/src/components/Footer.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export const Footer = () => {
   return (
-    <div className="bg-white dark:bg-gray-800 w-full pb-8 border-t shadow">
-      <div className="max-w-screen-xl px-4 mx-auto">
-        <div className="text-center text-gray-500 dark:text-gray-200 pt-10 sm:pt-6 font-light flex items-center justify-center">
+    <footer className="w-full border-t bg-white pb-8 shadow dark:bg-gray-800">
+      <div className="mx-auto max-w-screen-xl px-4">
+        <div className="flex items-center justify-center pt-10 text-center font-light text-gray-500 dark:text-gray-200 sm:pt-6">
           <p>
             © 2023 Kurumsal Java dersi için hazırlanan tüm materyaller Kurumsal
             &quot;Kurumsal Java&quot; şirketine aittir ve telif hakları
@@ -12,6 +12,6 @@ export const Footer = () => {
           </p>
         </div>
       </div>
-    </div>
+    </footer>
   );
 };
