5cf23453c53f67b280fbf322229f8b6a2e89c11e
Muhammed Talha Şahin <muhammedtalhasahin.tr@gmail.com>
2023-06-05 23:52:41 +0300

    Internship Evaluate Form ekranı
    Internship Evaluate Form ekranı oluşturuldu
    gerekli api'lar tanımlandı

--- clients/company-supervisor/src/components/InternshipRow.tsx
+++ clients/company-supervisor/src/components/InternshipRow.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState, ReactNode } from 'react';
 import { Internship } from '../types/InternshipType';
 import Modal from '../components/StudentInfo';
+import InternshipEvaluateForm from '../components/InternshipEvaluateForm';
 
-export const InternshipRow = ({ internship }: { internship: Internship }) => {
+export const InternshipRow = ({ internship, company }: { internship: Internship, company: number }) => {
   const [hoveredButton, setHoveredButton] = useState<string | null>(null);
   const [popUpScreen, setPopUpScreen] = useState<ReactNode>(<></>);
   const [popUpState, setPopUpState] = useState<boolean>(false);
@@ -13,7 +14,7 @@ export const InternshipRow = ({ internship }: { internship: Internship }) => {
       REJECTED: 'bg-red-200 p-20 rounded-full',
       PENDING: 'bg-yellow-200 p-20 rounded-full',
     };
-  
+
     return statusClasses[status] || '';
   };
 
@@ -22,18 +23,31 @@ export const InternshipRow = ({ internship }: { internship: Internship }) => {
   };
   const handleStudentInfo = () => {
     setPopUpState(true);
-    console.log("Running handleStudentInfo");
-    setPopUpScreen(<Modal _student={internship.student} isOpen={popUpState} onClose={onClosePopUp}></Modal>);
+    console.log('Running handleStudentInfo');
+    setPopUpScreen(
+      <Modal
+        _student={internship.student}
+        isOpen={popUpState}
+        onClose={onClosePopUp}
+      ></Modal>
+    );
   };
   const handleInternshipBooks = () => {
     setPopUpState(true);
-    console.log("Running handleInternshipBooks");
+    console.log('Running handleInternshipBooks');
     setPopUpScreen(<h2>STAJ BİLGİLERİ</h2>);
   };
   const handleCompanyEvaluation = () => {
     setPopUpState(true);
-    console.log("Running handleCompanyEvaluation");
-    setPopUpScreen(<h2>ŞİRKET DEĞERLENDİRMESİ</h2>);
+    console.log('Running handleCompanyEvaluation');
+    setPopUpScreen(
+      <InternshipEvaluateForm
+       _internshipId={internship.id}
+       _companyId={company}
+        isOpen={popUpState}
+        onClose={onClosePopUp}
+      ></InternshipEvaluateForm>
+    );
   };
   const onClosePopUp = () => {
     setPopUpState(false);
@@ -42,11 +56,14 @@ export const InternshipRow = ({ internship }: { internship: Internship }) => {
   const buttonHoverPopUp = (hoverText: string | null) => {
     return (
       <>
-        <svg className="hidden md:inline-block ml-1 absolute top-4 left-1/2 -translate-x-1/2 mt-2 " viewBox="0 0 20 20" >
+        <svg
+          className="absolute left-1/2 top-4 ml-1 mt-2 hidden -translate-x-1/2 md:inline-block "
+          viewBox="0 0 20 20"
+        >
           <path d="M10 0L20 20H0L10 0Z" fill=" bg-indigo-500" />
         </svg>
         <span
-          className={`hidden md:inline-block ml-1 absolute top-8 left-1/2 -translate-x-1/2 bg-indigo-500 py-1 px-2 rounded-md text-xs text-white whitespace-no-wrap transition-opacity duration-300 opacity-100`}
+          className={`whitespace-no-wrap absolute left-1/2 top-8 ml-1 hidden -translate-x-1/2 rounded-md bg-indigo-500 px-2 py-1 text-xs text-white opacity-100 transition-opacity duration-300 md:inline-block`}
           style={{ top: 'calc(100% + 8px)' }}
         >
           {hoverText}
@@ -57,31 +74,35 @@ export const InternshipRow = ({ internship }: { internship: Internship }) => {
 
   return (
     <tr>
-      <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
-        <p className="text-gray-900 whitespace-no-wrap">
+      <td className="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+        <p className="whitespace-no-wrap text-gray-900">
           {internship.student.name}
         </p>
       </td>
-      <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
-        <p className="text-gray-900 whitespace-no-wrap">
+      <td className="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+        <p className="whitespace-no-wrap text-gray-900">
           {internship.student.surname}
         </p>
       </td>
-      <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
-        <p className="text-gray-900 whitespace-no-wrap">{internship.days}</p>
+      <td className="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+        <p className="whitespace-no-wrap text-gray-900">{internship.days}</p>
       </td>
-      <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
-        <p className="text-gray-900 whitespace-no-wrap">
+      <td className="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+        <p className="whitespace-no-wrap text-gray-900">
           {new Date(internship.startDate).toLocaleDateString('en-GB')}
         </p>
       </td>
-      <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
-        <p className="text-gray-900 whitespace-no-wrap">
+      <td className="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+        <p className="whitespace-no-wrap text-gray-900">
           {new Date(internship.endDate).toLocaleDateString('en-GB')}
         </p>
       </td>
-      <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
-        <span className={`relative inline-block px-3 py-1 font-semibold leading-tight ${getStatusColor(internship.status)}`}>
+      <td className="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+        <span
+          className={`relative inline-block px-3 py-1 font-semibold leading-tight ${getStatusColor(
+            internship.status
+          )}`}
+        >
           <span
             aria-hidden="true"
             className="absolute inset-0 rounded-full opacity-50"
@@ -89,63 +110,83 @@ export const InternshipRow = ({ internship }: { internship: Internship }) => {
           <span className="relative">{internship.status}</span>
         </span>
       </td>
-      <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
+      <td className="border-b border-gray-200 bg-white px-5 py-5 text-sm">
         <div className="flex space-x-3">
           <button
-            className="text-indigo-600 hover:text-indigo-900 relative"
+            className="relative text-indigo-600 hover:text-indigo-900"
             onMouseEnter={() => handleButtonHover('studentInfo')}
             onMouseLeave={() => handleButtonHover(null)}
             onClick={() => handleStudentInfo()}
           >
-            <svg className="w-6 h-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            <svg
+              className="h-6 w-6"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
               <circle cx="12" cy="7" r="4"></circle>
               <path d="M2 20C2 14.4772 6.47715 10 12 10C17.5228 10 22 14.4772 22 20"></path>
             </svg>
-            {hoveredButton == 'studentInfo' && (
-              buttonHoverPopUp("Öğrenci Bilgileri")
-            )}
+            {hoveredButton == 'studentInfo' &&
+              buttonHoverPopUp('Öğrenci Bilgileri')}
           </button>
           <button
-            className="text-indigo-600 hover:text-indigo-900 relative"
+            className="relative text-indigo-600 hover:text-indigo-900"
             onMouseEnter={() => handleButtonHover('internshipBooks')}
             onMouseLeave={() => handleButtonHover(null)}
             onClick={() => handleInternshipBooks()}
           >
-            <svg className="w-6 h-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            <svg
+              className="h-6 w-6"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
               <path d="M9 19V5H21V19H9Z"></path>
               <path d="M9 3L9 19"></path>
               <path d="M21 3L9 3"></path>
               <path d="M3 3L3 19"></path>
             </svg>
-            {hoveredButton == 'internshipBooks' && (
-              buttonHoverPopUp("Staj Defteri")
-            )}
-
+            {hoveredButton == 'internshipBooks' &&
+              buttonHoverPopUp('Staj Defteri')}
           </button>
           <button
-            className="text-indigo-600 hover:text-indigo-900 relative"
+            className="relative text-indigo-600 hover:text-indigo-900"
             onMouseEnter={() => handleButtonHover('companyEvaluation')}
             onMouseLeave={() => handleButtonHover(null)}
             onClick={() => handleCompanyEvaluation()}
           >
-            <svg className="w-6 h-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            <svg
+              className="h-6 w-6"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
               <path d="M22 11.07V12a10 10 0 1 1-5.93-9.14"></path>
               <polyline points="22 4 12 14.01 9 11.01"></polyline>
             </svg>
-            {hoveredButton == 'companyEvaluation' && (
-              buttonHoverPopUp("Şirket Değerlendirmesi")
-            )}
+            {hoveredButton == 'companyEvaluation' &&
+              buttonHoverPopUp('Şirket Değerlendirmesi')}
           </button>
         </div>
       </td>
       {popUpState && (
-        <div className="fixed inset-0 flex items-center justify-center z-50">
+        <div className="fixed inset-0 z-50 flex items-center justify-center">
           <div className="absolute inset-0 bg-gray-900 opacity-50"></div>
-          <div className="bg-white rounded-lg p-8 z-50 flex flex-col items-center">
+          <div className="z-50 flex flex-col items-center rounded-lg bg-white p-8">
             {popUpScreen}
             <br />
             <button
-              className="mt-4 bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded"
+              className="mt-4 rounded bg-indigo-500 px-4 py-2 font-bold text-white hover:bg-indigo-600"
               style={{ backgroundColor: '#3A4F7A' }}
               onClick={onClosePopUp}
             >
