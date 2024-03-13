8f3c73b4b7cffee3855bf85c4f4cc921f6cfc262
İrem Kavakpınar <kavakpinarirem1@gmail.com>
2024-03-13 17:21:03 +0300

    5 numaralı script düzeltildi ve dummdata.sql dosyası olmadığından README düzenlendi.

--- db-scripts/imsdb5.sql
+++ db-scripts/imsdb5.sql
@@ -4,6 +4,5 @@ DROP TABLE IF EXISTS ims_user_types;
 
 ALTER TABLE IF EXISTS public.ims_users DROP COLUMN IF EXISTS user_type_id;
 
-#CLEAR TABLE BEFORE EXECUTE NEXT LINE
 ALTER TABLE IF EXISTS public.ims_users ADD COLUMN user_type character varying NOT NULL;
 
