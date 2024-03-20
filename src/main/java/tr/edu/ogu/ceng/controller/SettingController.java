55dbeeb8c1585921f48b47aba2749fdc5c3b4334
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-03-20 13:37:07 +0300

    Endpoint kontrol edildi, yorum eklendi.

--- src/main/java/tr/edu/ogu/ceng/controller/SettingController.java
+++ src/main/java/tr/edu/ogu/ceng/controller/SettingController.java
@@ -19,16 +19,29 @@ public class SettingController {
 	@Autowired
 	private SettingService settingService;
 
-	@PutMapping(value = "/{key}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<SettingDto> updateSetting(@PathVariable(name = "key") String key, @RequestBody SettingDto setting) {
-		SettingDto updatedSetting = settingService.updateSetting(key, setting);
-		return ResponseEntity.ok(updatedSetting);
-	}
-
+	/**
+	 * Gets value of the given key.
+	 * @param key - Key which is its value to be changed.
+	 * @return SettingDto
+	 */
 	@GetMapping("/{key}")
 	public ResponseEntity<SettingDto> getSetting(@PathVariable(name = "key") String key) {
 		SettingDto setting = settingService.getSetting(key);
 
 		return ResponseEntity.ok(setting);
 	}
+
+	/**
+	 * Updates setting with key value pair.
+	 * @param key - Key which is its value to be changed.
+	 * @body SettingDto
+	 * @return SettingDto
+	 */
+	@PutMapping(value = "/{key}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<SettingDto> updateSetting(@PathVariable(name = "key") String key, @RequestBody SettingDto setting) {
+		SettingDto updatedSetting = settingService.updateSetting(key, setting);
+		return ResponseEntity.ok(updatedSetting);
+	}
+
+
 }
