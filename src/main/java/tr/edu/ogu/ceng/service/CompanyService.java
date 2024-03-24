1b94db985bcd8e6288bac2edf2b7ad2edd9368a8
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-03-24 10:29:45 +0300

    Company service için prototip mesajlar ve servis düzenlemesi

--- src/main/java/tr/edu/ogu/ceng/service/CompanyService.java
+++ src/main/java/tr/edu/ogu/ceng/service/CompanyService.java
@@ -1,9 +1,12 @@
 package tr.edu.ogu.ceng.service;
 
 import java.time.LocalDateTime;
+import java.util.Locale;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -12,6 +15,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import tr.edu.ogu.ceng.dao.CompanyRepository;
 import tr.edu.ogu.ceng.dto.CompanyDto;
+import tr.edu.ogu.ceng.internationalization.MessageResource;
 import tr.edu.ogu.ceng.model.Company;
 import tr.edu.ogu.ceng.service.Exception.EntityNotFoundException;
 import tr.edu.ogu.ceng.service.Exception.InvalidArgumentException;
@@ -24,19 +28,21 @@ public class CompanyService {
 	@Autowired
 	private CompanyRepository companyRepository;
 
+	@Autowired
+	private MessageResource messageResource;
+
 	public Page<CompanyDto> getAllCompanies(Pageable pageable) {
 		ModelMapper modelMapper = new ModelMapper();
 		Page<Company> companies = companyRepository.findAll(pageable);
 		Page<CompanyDto> companyDtos = companies.map(company -> modelMapper.map(company, CompanyDto.class));
-		log.info("Pageable companies fetched");
+		log.info(messageResource.getMessage("company.service.pageable.companies.fetched"));
 		return companyDtos;
-
 	}
 
 	public CompanyDto updateCompany(CompanyDto companyDto) {
 		try {
 			if (!companyRepository.existsById(companyDto.getId())) {
-				log.warn("There is no company with the entered ID.");
+				log.warn(messageResource.getMessage("company.service.warn.no.company.with.id", companyDto.getId()));
 				throw new tr.edu.ogu.ceng.service.Exception.EntityNotFoundException();
 			}
 			ModelMapper modelMapper = new ModelMapper();
@@ -45,8 +51,8 @@ public class CompanyService {
 			company.setUpdateDate(dateTime);
 			company.setCreateDate(companyRepository.getById(companyDto.getId()).getCreateDate());
 			company = companyRepository.save(company);
-			log.info("Company with ID {} has been updated: {}, {}", company.getId(), company.getName(),
-					company.getDescription());
+			log.info(messageResource.getMessage("company.service.info.company.updated", company.getId(), company.getName(),
+					company.getDescription()));
 			return modelMapper.map(company, CompanyDto.class);
 		} catch (EntityNotFoundException e) {
 			throw new tr.edu.ogu.ceng.service.Exception.EntityNotFoundException();
@@ -56,7 +62,7 @@ public class CompanyService {
 	public CompanyDto getCompany(long id) throws EntityNotFoundException {
 		Company company = companyRepository.findById(id).orElse(null);
 		if (company == null) {
-			log.warn("Company not found with id {}", id);
+			log.warn(messageResource.getMessage("company.service.warn.company.not.found", id));
 			throw new EntityNotFoundException();
 		}
 		ModelMapper modelMapper = new ModelMapper();
@@ -65,7 +71,7 @@ public class CompanyService {
 
 	public Page<CompanyDto> searchCompanies(String name, Pageable pageable) {
 		if (name.length() < 3) {
-			log.warn("Lenght of company name is less than 3 characters long.");
+			log.warn(messageResource.getMessage("company.service.warn.company.name.length.less.than.three"));
 			throw new InvalidArgumentException("Name should be at least 3 characters long.");
 		}
 		ModelMapper modelMapper = new ModelMapper();
@@ -82,26 +88,27 @@ public class CompanyService {
 			company.setCreateDate(dateTime);
 			company.setUpdateDate(dateTime);
 			companyRepository.save(company);
-			log.info("The company has been added successfully.");
+			log.info(messageResource.getMessage("company.service.info.company.added"));
 			return modelMapper.map(company, CompanyDto.class);
 		} catch (Exception e) {
-			log.error("Failed to add company. Error message: {}", e.getMessage());
+			log.error(messageResource.getMessage("company.service.error.failed.to.add.company", e.getMessage()));
 			throw e;
 		}
 	}
 
 	public boolean deleteCompany(long id) {
 		if (!companyRepository.existsById(id)) {
-			log.warn("Company not found with id {}", id);
+			log.warn(messageResource.getMessage("company.service.warn.company.not.found.with.id", id));
 			return false;
 		}
 
 		companyRepository.deleteById(id);
-		log.info("Company has been deleted successfully.");
+		log.info(messageResource.getMessage("company.service.info.company.deleted"));
 		return true;
 	}
 
 	public Long countCompanies() {
 		return companyRepository.count();
 	}
+
 }
