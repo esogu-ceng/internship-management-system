b33cc72d010f48aa18505d81636d1defb5db9639
Kerem Okumus <96080839+Keremovsky@users.noreply.github.com>
2024-03-26 12:22:20 +0300

    company-supervisor footer düzeltildi

--- clients/company-supervisor/src/components/Footer.tsx
+++ clients/company-supervisor/src/components/Footer.tsx
@@ -6,7 +6,7 @@ export const Footer = () => {
       <div className="mx-auto max-w-screen-xl px-4">
         <div className="flex items-center justify-center pt-10 text-center font-light text-gray-500 dark:text-gray-200 sm:pt-6">
           <p>
-            © Osmangazi Üniversitesi Bilgisayar Mühendisliği öğrencileri tarafından hazırlanmıştır. Tüm hakları Osmangazi Üniversitesi Bilgisayar Mühendisliği'ne aittir. Tag: v1.0
+            © Osmangazi Üniversitesi Bilgisayar Mühendisliği öğrencileri tarafından hazırlanmıştır. Tüm hakları Osmangazi Üniversitesi Bilgisayar Mühendisliği&apos;ne aittir. Tag: v1.0
           </p>
         </div>
       </div>
