1e6fe6f54a601911f7530c8487169355b33408de
Bahar Togay <bahartogaay@gmail.com>
2024-03-30 16:16:14 +0300

    internship status enum değerleri güncellendi.

--- src/main/java/tr/edu/ogu/ceng/enums/InternshipStatus.java
+++ src/main/java/tr/edu/ogu/ceng/enums/InternshipStatus.java
@@ -1,5 +1,16 @@
 package tr.edu.ogu.ceng.enums;
 
 public enum InternshipStatus {
-	PENDING, APPROVED, REJECTED
+	APPLIED, 			// staj başvurusu yapıldı ve sonuç bekleniyor.
+	COMPANY_APPROVED, 	// firma staj başlangıcını onayladı.
+	FACULTY_APPROVED,	// fakülte staj başlangıcını onayladı.
+	ONGOING, 			// staj yapılmakta.
+	COMPANY_EVALUATION_STAGE,	// staj firma değerlendirme formunun eklenmesi bekleniyor.
+	FACULTY_EVALUATION_STAGE, // staj firma tarafından onaylandı, fakültenin onayını bekliyor.
+	SUCCESS, 			// fakülte stajın bitişini onayladı ve resmi işlemler tamamlandı.
+	
+	FACULTY_REJECTED,	// fakülte stajı reddetti.
+	COMPANY_REJECTED, 	// firma stajı reddetti.
+	FACULTY_INVALID,	// fakülte stajı geçersiz buldu.
+	CANCELED,			// staj fakülte tarafından iptal edildi.
 }
