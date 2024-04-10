d9a93a8d9d1c5b90a2197da4d525984535e34377
huseyindedeyilmaz <hy1384862@gmail.com>
2024-04-10 12:43:24 +0300

    InternshipJournalsType.tsx oluşturuldu.

---
+++ clients/company-supervisor/src/types/InternshipJournalsType.tsx
@@ -0,0 +1,14 @@
+export type InternshipJournalsType = {
+    id: number;
+    internshipId: number;
+    companySupervisorId: number;
+    operationTime: number;
+    journal: string;
+    unitName: string;
+    startingDate: string;
+    endDate: string;
+    createDate: string;
+    updateDate: string;
+    confirmation: boolean;
+  };
+  
